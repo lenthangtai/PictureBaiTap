378233e3db778d7bd042a45f631c25bad8880f8d
Tài Lê Nguyễn Thắng <lenthangtai@gmail.com>
2022-09-14 08:59:45 +0700

    Add project files.

---
+++ PictureBaiTap/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace PictureBaiTap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageView = new System.Windows.Forms.PictureBox();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnInputPath = new System.Windows.Forms.Button();
+            this.pnlFooter = new System.Windows.Forms.Panel();
+            this.txtSave = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageView)).BeginInit();
+            this.pnlHeader.SuspendLayout();
+            this.pnlFooter.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ImageView
+            // 
+            this.ImageView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ImageView.Location = new System.Drawing.Point(0, 0);
+            this.ImageView.Name = "ImageView";
+            this.ImageView.Size = new System.Drawing.Size(442, 628);
+            this.ImageView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ImageView.TabIndex = 4;
+            this.ImageView.TabStop = false;
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.Controls.Add(this.txtPath);
+            this.pnlHeader.Controls.Add(this.btnInputPath);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(442, 26);
+            this.pnlHeader.TabIndex = 7;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtPath.Location = new System.Drawing.Point(43, 0);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(399, 26);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnInputPath
+            // 
+            this.btnInputPath.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnInputPath.Location = new System.Drawing.Point(0, 0);
+            this.btnInputPath.Name = "btnInputPath";
+            this.btnInputPath.Size = new System.Drawing.Size(43, 26);
+            this.btnInputPath.TabIndex = 0;
+            this.btnInputPath.Text = "...";
+            this.btnInputPath.UseVisualStyleBackColor = true;
+            this.btnInputPath.Click += new System.EventHandler(this.btnInputPath_Click);
+            // 
+            // pnlFooter
+            // 
+            this.pnlFooter.Controls.Add(this.txtSave);
+            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlFooter.Location = new System.Drawing.Point(0, 599);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(442, 29);
+            this.pnlFooter.TabIndex = 8;
+            // 
+            // txtSave
+            // 
+            this.txtSave.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtSave.Location = new System.Drawing.Point(0, 0);
+            this.txtSave.Name = "txtSave";
+            this.txtSave.Size = new System.Drawing.Size(442, 29);
+            this.txtSave.TabIndex = 0;
+            this.txtSave.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSave_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.pnlFooter);
+            this.panel1.Controls.Add(this.ImageView);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(442, 628);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblCount);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.panel2.Location = new System.Drawing.Point(0, 578);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(442, 21);
+            this.panel2.TabIndex = 9;
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblCount.Location = new System.Drawing.Point(430, 0);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(12, 17);
+            this.lblCount.TabIndex = 0;
+            this.lblCount.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 628);
+            this.Controls.Add(this.pnlHeader);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ImageView)).EndInit();
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.pnlFooter.ResumeLayout(false);
+            this.pnlFooter.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImageView;
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnInputPath;
+        private System.Windows.Forms.Panel pnlFooter;
+        private System.Windows.Forms.TextBox txtSave;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ PictureBaiTap/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBaiTap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> locFile;
+        FolderBrowserDialog fd = new FolderBrowserDialog();
+        int counter = 0;
+
+        public void HienAnh()
+        {
+            ImageView.Image = Image.FromFile(locFile[counter]);
+            lblCount.Text = Convert.ToInt32(counter + 1) + "/" + locFile.Count.ToString();
+        }
+        private void btnInputPath_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] filterFile = { "*.png","*.jpg", };
+                fd.ShowDialog();
+                //locFile = Directory.GetFiles(fd.SelectedPath, "*").Where(file => file.ToLower().EndsWith("jpg") || file.ToLower().EndsWith("png")).ToList();
+                //locFile = Directory.GetFiles(fd.SelectedPath, "*.jpg").Concat(Directory.GetFiles(fd.SelectedPath, "*.png")).ToList();
+                locFile = filterFile.SelectMany(file => Directory.GetFiles(fd.SelectedPath, file)).ToList();
+                if (locFile.Count() == 0)
+                {
+                    MessageBox.Show("This Folder Don't Have A Picture!!!\nPlease, Pick Folder Another.", "Alert Title", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    HienAnh();
+                    //Tạo folder Result
+                    DirectoryInfo dir = new DirectoryInfo(fd.SelectedPath + "\\" + "result");
+                    if (dir.Exists) { } else { dir.Create(); }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtSave_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ImageView.Image.Save(fd.SelectedPath + "\\" + "result\\" + counter.ToString().PadLeft(6, '0') + "_" + txtSave.Text.Trim() + ".jpg");
+                counter += 1;
+                if (counter == locFile.Count)
+                {
+                    MessageBox.Show("Complete!");
+                    this.Close();
+                    return;
+                }
+                else
+                {
+                    HienAnh();
+                    txtSave.Clear();
+                }
+            }
+        }
+
+
+    }
+}
